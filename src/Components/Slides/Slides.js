068e94ab2a48484c52a8563d7a453b6849134f97
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-04-23 17:28:37 +0100

    Add asparagus fern images and the image carousel

--- src/Components/Slides/Slides.js
+++ src/Components/Slides/Slides.js
@@ -6,11 +6,18 @@ import monstera2 from "./monstera/2.jpg"
 import monstera3 from "./monstera/3.jpg"
 import monstera4 from "./monstera/4.jpg"
 import monstera5 from "./monstera/5.2.jpg"
-
 import monstera7 from "./monstera/7.jpg"
 import monstera8 from "./monstera/8.jpg"
 import monstera9 from "./monstera/9.jpg"
 import monstera10 from "./monstera/10.jpg"
+import fern1 from "./fern/1.jpg"
+import fern2 from "./fern/2.jpg"
+import fern3 from "./fern/3.jpg"
+import fern4 from "./fern/4.jpg"
+import fern5 from "./fern/5.jpg"
+import fern6 from "./fern/6.jpg"
+import fern7 from "./fern/7.jpg"
+import fern8 from "./fern/8.jpg"
 
 
 const monsteras = [
@@ -23,6 +30,17 @@ const monsteras = [
   monstera8,
   monstera9,
   monstera10
+];
+
+const ferns = [
+  fern1,
+  fern2,
+  fern3,
+  fern4,
+  fern5,
+  fern6,
+  fern7,
+  fern8
 ];
 
 const zoomOutProperties = {
@@ -41,6 +59,12 @@ const Slide1 = () => {
           {
             monsteras.map((each, index) => <img key={index} src={each} alt="monstera"/>)
           }
+
+        </Zoom>
+          <Zoom {...zoomOutProperties} >
+        {
+          ferns.map((each, index) => <img key={index} src={each} alt="monstera"/>)
+        }
         </Zoom>
       </div>
     )
